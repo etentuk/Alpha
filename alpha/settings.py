6c5c7a58483be2cbc313cbe95b92b8ef6a7cde04
Etentuk <etentuke@gmail.com>
2021-12-30 09:21:51 +0100

    hopefully final

--- alpha/settings.py
+++ alpha/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'alpha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'bugtracker/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- alpha/urls.py
+++ alpha/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from dj_rest_auth.views import PasswordResetConfirmView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -29,4 +30,5 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('bugtracker.api.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
