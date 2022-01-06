3db8148280e672ad9ecb1de001599dc582ee61fb
Etentuk <etentuke@gmail.com>
2022-01-06 01:24:01 +0100

    revert to sqlitedb

--- alpha/settings.py
+++ alpha/settings.py
@@ -112,7 +112,12 @@ WSGI_APPLICATION = 'alpha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 AUTH_USER_MODEL = "bugtracker.User"
 
