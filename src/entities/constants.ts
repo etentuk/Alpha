78b09575972f4b9083de24b00180c96c97230068
Etentuk <etentuke@gmail.com>
2022-01-06 02:41:35 +0100

    DB_HOST config

--- src/entities/constants.ts
+++ src/entities/constants.ts
@@ -19,5 +19,12 @@ export const ticketPriority = ['Low', 'Medium', 'High'];
 export const userRoles = ['ADMIN', 'PROJECT_MANAGER', 'DEVELOPER', 'SUBMITTER'];
 
 export const demoUsers = userRoles.map((r) => 'demo_' + r.toLowerCase());
-export const DB_AUTH = `${process.env.REACT_APP_DB_HOST}/dj-rest-auth/`;
-export const DB_API = `${process.env.REACT_APP_DB_HOST}/api/`;
+
+let DB_HOST = '';
+
+if (process.env.NODE_ENV === 'development')
+    DB_HOST = process.env.REACT_APP_DB_HOST!;
+
+export const DB_AUTH = `${DB_HOST}dj-rest-auth/`;
+
+export const DB_API = `${DB_HOST}api/`;
